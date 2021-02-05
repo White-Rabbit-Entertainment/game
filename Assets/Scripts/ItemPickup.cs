a84489f33bcb3806c0fb41b2cbf01820942cbee8
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-05 11:14:01 +0000

    added basic item pickup functionality

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Transform pickupDestination;
+
+    void OnMouseDown() {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        transform.position = pickupDestination.position;
+        transform.parent = GameObject.Find("Item Pickup Destination").transform;
+    }
+
+    void OnMouseUp() {
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+        transform.parent = null;
+    }
+}
