558fa21af4f37f3e72617c093936b1cfd47d04ca
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-07 11:43:36 +0000

    added basic movement tests

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement
+{
+    private float groundDistance = 0.4f;
+    public float speed;
+    public float gravity;
+    public float jumpHeight;
+    public float y = 0f;
+
+    public Movement(float speed, float gravity, float jumpHeight) {
+        this.speed = speed;
+        this.gravity = gravity;
+        this.jumpHeight = jumpHeight;
+    }
+
+    public Vector3 Calculate(float x, float z, bool isJumping, bool isGrounded, float deltaTime) {
+
+        //resets falling velocity if player is grounded
+        if (isGrounded && y < 0) {
+            y = -2f;
+        }
+
+        //makes the player jump if they are on the ground
+        if (isJumping && isGrounded) {
+            y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //handles gravity
+        y += gravity * deltaTime;
+
+        //combining 2D transform and y velocity
+        Vector3 move = new Vector3(x, 0, z) * speed;
+        move.y = y;  //adding gravity transform
+
+        return move * deltaTime;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,39 +11,26 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -19.62f;
     public float jumpHeight = 3f;
-
-    private Vector3 velocity;
-    private bool isGrounded;
+    public Movement movement;
   
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = new Movement(speed, gravity, jumpHeight);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //resets falling velocity if player is grounded
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0) {
-            velocity.y = -2f;
-        }
-
-        //moving player in 2D plane
+        //get user input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        playerController.Move(move * speed * Time.deltaTime);
+        bool isJumping = Input.GetButtonDown("Jump");
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        //makes the player jump if they are on the ground
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        //handles gravity
-        velocity.y += gravity * Time.deltaTime;
-        playerController.Move(velocity * Time.deltaTime);
+        //apply movement
+        Vector3 move = movement.Calculate(x, z, isJumping, isGrounded, Time.deltaTime);
+        Vector3 finalMove = move.x * transform.right + move.y * transform.up + move.z * transform.forward;
+        playerController.Move(finalMove);
     }
 }
