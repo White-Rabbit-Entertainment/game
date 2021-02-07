2ff89a49fb96674d1ead0e1f22141db108f20883
JDBryan <47214792+JDBryan@users.noreply.github.com>
2021-02-07 11:57:51 +0000

    added basic movement tests (#21)
    * added basic movement tests
    
    * Update Assets/Scripts/Movement.cs
    
    Co-authored-by: James Elgar <jamesnelgar@gmail.com>
    
    * Update Assets/Scripts/PlayerMovement.cs
    
    Co-authored-by: James Elgar <jamesnelgar@gmail.com>
    
    * removed AssertTrue
    
    * removed built version
    
    Co-authored-by: Joe Bryan <dn18923@bristol.ac.uk>
    Co-authored-by: James Elgar <jamesnelgar@gmail.com>

---
+++ Assets/Tests/Editor/MovementTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Editor {
+    public class MovementTests {
+        [Test]
+        public void MoveXTest() {
+            Movement m = new Movement(1,0,0);
+            Assert.AreEqual(1, m.Calculate(1,0,false,true,1).x);
+        }
+
+        [Test]
+        public void MoveZTest() {
+            Movement m = new Movement(1,0,0);
+            Assert.AreEqual(1, m.Calculate(0,1,false,true,1).z);
+        }
+
+        [Test]
+        public void FallingTest() {
+            Movement m = new Movement(0,-10,0);
+            for (int i = 0; i < 10; i++) {
+                Assert.AreEqual(-0.1f * (i+1), m.Calculate(0,0,false,false,0.1f).y);
+            }
+        }
+
+        [Test]
+        public void JumpingTest() {
+            Movement m = new Movement(0,-10,5);
+            Assert.IsTrue(Mathf.Approximately(0.9f, m.Calculate(0,0,true,true,0.1f).y));
+        }
+
+        [Test]
+        public void JumpingNotGroundedTest() {
+            Movement m = new Movement(0,-10,5);
+            Assert.AreEqual(-0.1f, m.Calculate(0,0,true,false,0.1f).y);
+        }
+    }
+}
