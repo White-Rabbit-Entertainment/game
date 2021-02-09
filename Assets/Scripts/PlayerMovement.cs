92fa90397c8519aa1a54957ebf8639eab3aee9d5
Theoajc <Theoajclarke@outlook.com>
2021-02-09 14:21:51 +0000

    Sprinting added with basic stamina that uses a simple decay and regenerate function

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,25 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public CharacterController playerController;
-    public float speed = 12f;
+    public float speed = 5f;
+    public float staminaStart = 10f;
+    public float stamina = 10f;
+    public float staminaDepletionRate = 2;
+    public float staminaRegenerationRate = 2;
+    public float sprintSpeed = 24f;
     public float gravity = -19.62f;
     public float jumpHeight = 3f;
     private Movement movement;
+    private Movement slowMovement;
+    private Movement fastMovement;
+
   
     // Start is called before the first frame update
     void Start()
     {
-        movement = new Movement(speed, gravity, jumpHeight);
+        slowMovement = new Movement(speed, gravity, jumpHeight);
+        fastMovement = new Movement(sprintSpeed, gravity, jumpHeight);
+        movement = slowMovement;
     }
 
     // Update is called once per frame
@@ -32,5 +42,39 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = movement.Calculate(x, z, isJumping, isGrounded, Time.deltaTime);
         Vector3 finalMove = move.x * transform.right + move.y * transform.up + move.z * transform.forward;
         playerController.Move(finalMove);
+        SprintOrWalk();
+
     }
+
+    //Decide whether player is walking or sprinting
+    void SprintOrWalk() {
+        //Left shift triggers sprint if player has stamina
+         if (Input.GetKey("left shift"))
+        {   
+            // print("sprinting");
+            if (stamina <= 0.01) {
+                //If player hasn't got enough stamina, no sprint
+                movement = slowMovement;
+                // print("Tired");
+            }
+            else
+            {   
+                //Otherwise sprint
+                // print("sprinting");
+                movement = fastMovement;
+            }
+            //Deplete stamina because sprint has been attempted (decay function can be swapped out)
+            stamina = stamina - (1 / staminaDepletionRate) * Time.deltaTime;
+        }
+        else 
+        {   
+            //No sprint attempted so walking, stamina regenerates
+            movement = slowMovement;
+            stamina = stamina + (1 * staminaRegenerationRate) * Time.deltaTime;
+        }
+        //Don't let stamina get outside of zero or max value
+        this.stamina = Mathf.Clamp(this.stamina, 0, staminaStart);
+        // print(stamina);
+    }
+
 }
