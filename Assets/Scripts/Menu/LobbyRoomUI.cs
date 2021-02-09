d6988de3179fe58fd9f2efff4bd0a637ff541196
James Elgar <jamesnelgar@gmail.com>
2021-02-09 15:07:26 +0000

    Feature/22/photon networking (#43)
    * Basic lobby and create room
    
    * Basic lobby and create room
    
    * Lobby scene player list
    
    * Print number of players in the room
    
    * Added photon instantiate player script
    
    * Photon player
    
    * Destroy view script if not mine
    
    * Destroyed lookscript if not me
    
    * Destroyed other palyers camera on start
    
    * Dont destory player on scene change
    
    * Destroy movement script if not me
    
    Co-authored-by: Joe Bryan <dn18923@bristol.ac.uk>

---
+++ Assets/Scripts/Menu/LobbyRoomUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyRoomUI : MonoBehaviour
+{
+
+    public Text playerList;
+    
+    public Button startGame; 
+    
+    private NetworkManager network;
+    private string gameScene = "GameScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       network = new NetworkManager();
+        
+       // Setup start game button
+       startGame.onClick.AddListener(OnClickStartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      SetText();
+    }
+
+    void SetText() {
+      playerList.text = network.GetPlayers().Count.ToString();
+    }
+
+    void OnClickStartGame() {
+      network.ChangeScene(gameScene);
+    }
+}
