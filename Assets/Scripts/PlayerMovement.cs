c1b988a65428db03888f49f2187f42477032bb5c
Theoajc <Theoajclarke@outlook.com>
2021-02-10 13:25:12 +0000

    all sprinting and stamina logic moved to movement class

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     public CharacterController playerController;
     public float speed = 5f;
+    public float sprintFactor = 2f;
     public float stamina = 1f;
     public float staminaDepletionRate = 2;
     public float staminaRegenerationRate = 2;
@@ -21,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        movement = new Movement(speed, gravity, jumpHeight);
+        movement = new Movement(speed, gravity, jumpHeight, sprintFactor, stamina, staminaDepletionRate, staminaRegenerationRate);
     }
 
     // Update is called once per frame
@@ -33,38 +34,12 @@ public class PlayerMovement : MonoBehaviour
         bool isJumping = Input.GetButtonDown("Jump");
         bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         bool isSprinting = Input.GetKey("left shift");
-        isSprinting = staminaHandler(isSprinting);
+        // isSprinting = staminaHandler(isSprinting);
         //apply movement
         Vector3 move = movement.Calculate(x, z, isJumping, isGrounded, isSprinting, Time.deltaTime);
         Vector3 finalMove = move.x * transform.right + move.y * transform.up + move.z * transform.forward;
         playerController.Move(finalMove);
 
-    }
-
-    //Decide whether player is walking or sprinting
-    bool staminaHandler(bool isSprinting) {
-        //Left shift triggers sprint if player has stamina
-         if (isSprinting)
-        {   
-            if (stamina == 0) {
-                //If player hasn't got enough stamina, no sprint
-                return false;
-            }
-            //Deplete stamina because sprint has been attempted (decay function can be swapped out)
-            stamina = stamina - (1 / staminaDepletionRate) * Time.deltaTime;
-        }
-        else 
-        {   
-            //No sprint attempted so walking, stamina regenerates
-            stamina = stamina + (1 * staminaRegenerationRate) * Time.deltaTime;
-            //Don't let stamina get outside of zero or max value
-            this.stamina = Mathf.Clamp(this.stamina, 0, 1);
-            return false;
-        }
-        //Don't let stamina get outside of zero or max value
-        this.stamina = Mathf.Clamp(this.stamina, 0, 1);
-        // print(stamina);
-        return true;
     }
 
 }
--- Assets/Tests/Editor/MovementTest.cs
+++ Assets/Tests/Editor/MovementTest.cs
@@ -5,31 +5,36 @@ namespace Tests.Editor {
     public class MovementTests {
         [Test]
         public void MoveXTest() {
-            Movement m = new Movement(1,0,0);
+            Movement m = new Movement(1,0,0,2,1,2,2);
             Assert.AreEqual(1, m.Calculate(1,0,false,true,false,1).x);
         }
 
         [Test]
         public void MoveXSprintingTest() {
-            Movement m = new Movement(1,0,0);
+            Movement m = new Movement(1,0,0,2,1,2,2);
             Assert.AreEqual(2, m.Calculate(1,0,false,true,true,1).x);
+        }
+
+        public void MoveXSprintingNoStaminaTest() {
+            Movement m = new Movement(1,0,0,2,0,2,2);
+            Assert.AreEqual(1, m.Calculate(1,0,false,true,true,1).x);
         }
 
         [Test]
         public void MoveZTest() {
-            Movement m = new Movement(1,0,0);
+            Movement m = new Movement(1,0,0,2,1,2,2);
             Assert.AreEqual(1, m.Calculate(0,1,false,true,false,1).z);
         }
 
          [Test]
         public void MoveZSprintingTest() {
-            Movement m = new Movement(1,0,0);
+            Movement m = new Movement(1,0,0,2,1,2,2);
             Assert.AreEqual(2, m.Calculate(0,1,false,true,true,1).z);
         }
 
         [Test]
         public void FallingTest() {
-            Movement m = new Movement(0,-10,0);
+            Movement m = new Movement(0,-10,0,2,1,2,2);
             for (int i = 0; i < 10; i++) {
                 Assert.AreEqual(-0.1f * (i+1), m.Calculate(0,0,false,false,false,0.1f).y);
             }
@@ -37,13 +42,13 @@ namespace Tests.Editor {
 
         [Test]
         public void JumpingTest() {
-            Movement m = new Movement(0,-10,5);
+            Movement m = new Movement(0,-10,5,2,1,2,2);
             Assert.IsTrue(Mathf.Approximately(0.9f, m.Calculate(0,0,true,true,false,0.1f).y));
         }
 
         [Test]
         public void JumpingNotGroundedTest() {
-            Movement m = new Movement(0,-10,5);
+            Movement m = new Movement(0,-10,5,2,1,2,2);
             Assert.AreEqual(-0.1f, m.Calculate(0,0,true,false,false,0.1f).y);
         }
     }
