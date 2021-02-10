b00c399198edfb4f19ae83390db682072e0247d0
theoajc <48887209+theoajc@users.noreply.github.com>
2021-02-10 14:00:41 +0000

    Sprinting added with basic stamina (#46)
    * Sprinting added with basic stamina that uses a simple decay and regenerate function
    
    * Sprinting added with basic stamina that uses a simple decay and regenerate function
    
    * moved sprinting maths into movement class
    
    * all sprinting and stamina logic moved to movement class
    
    * all sprinting and stamina logic moved to movement class
    
    Co-authored-by: James Elgar <jamesnelgar@gmail.com>

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Movement
 {
     private float groundDistance = 0.4f;
     private float speed;
+    private float sprintFactor = 2f;
     private float gravity;
     private float jumpHeight;
     private float y = 0f;
+    public float stamina;
+    private float staminaDepletionRate;
+    private float staminaRegenerationRate;
 
-    public Movement(float speed, float gravity, float jumpHeight) {
+    public Movement(float speed, float gravity, float jumpHeight, float sprintFactor, float stamina, float staminaDepletionRate, float staminaRegenerationRate) {
         this.speed = speed;
         this.gravity = gravity;
         this.jumpHeight = jumpHeight;
+        this.sprintFactor = sprintFactor;
+        this.stamina = stamina;
+        this.staminaDepletionRate = staminaDepletionRate;
+        this.staminaRegenerationRate = staminaRegenerationRate;
     }
 
-    public Vector3 Calculate(float x, float z, bool isJumping, bool isGrounded, float deltaTime) {
+    public Vector3 Calculate(float x, float z, bool isJumping, bool isGrounded, bool isSprinting, float deltaTime) {
 
         //resets falling velocity if player is grounded
         if (isGrounded && y < 0) {
@@ -33,8 +42,33 @@ public class Movement
 
         //combining 2D transform and y velocity
         Vector3 move = new Vector3(x, 0, z) * speed;
-        move.y = y;  //adding gravity transform
 
+        isSprinting = staminaHandler(isSprinting, deltaTime);
+        if (isSprinting) move =  move * sprintFactor;
+        move.y = y;  //adding gravity transform
         return move * deltaTime;
+    }
+
+    private bool staminaHandler(bool isSprinting, float deltaTime) {
+        //Left shift triggers sprint if player has stamina
+         if (isSprinting)
+        {   
+            if (stamina == 0) {
+                //If player hasn't got enough stamina, no sprint
+                return false;
+            }
+            //Deplete stamina because sprint has been attempted (decay function can be swapped out)
+            stamina = stamina - (1 / staminaDepletionRate) * deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, 1);
+            return true;
+        }
+        else 
+        {   
+            //No sprint attempted so walking, stamina regenerates
+            stamina = stamina + (1 / staminaRegenerationRate) * deltaTime;
+            //Don't let stamina get outside of zero or max value
+            stamina = Mathf.Clamp(stamina, 0, 1);
+            return false;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,11 @@ public class PlayerMovement : MonoBehaviourPun
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public CharacterController playerController;
-    public float speed = 12f;
+    public float speed = 5f;
+    public float sprintFactor = 2f;
+    public float stamina = 1f;
+    public float staminaDepletionRate = 2;
+    public float staminaRegenerationRate = 2;
     public float gravity = -19.62f;
     public float jumpHeight = 3f;
     private Movement movement;
@@ -28,7 +32,7 @@ public class PlayerMovement : MonoBehaviourPun
     // Start is called before the first frame update
     void Start()
     {
-        movement = new Movement(speed, gravity, jumpHeight);
+        movement = new Movement(speed, gravity, jumpHeight, sprintFactor, stamina, staminaDepletionRate, staminaRegenerationRate);
     }
 
     // Update is called once per frame
@@ -38,10 +42,12 @@ public class PlayerMovement : MonoBehaviourPun
         float z = Input.GetAxis("Vertical");
         bool isJumping = Input.GetButtonDown("Jump");
         bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
+        bool isSprinting = Input.GetKey("left shift");
         //apply movement
-        Vector3 move = movement.Calculate(x, z, isJumping, isGrounded, Time.deltaTime);
+        Vector3 move = movement.Calculate(x, z, isJumping, isGrounded, isSprinting, Time.deltaTime);
         Vector3 finalMove = move.x * transform.right + move.y * transform.up + move.z * transform.forward;
         playerController.Move(finalMove);
+
     }
+
 }
