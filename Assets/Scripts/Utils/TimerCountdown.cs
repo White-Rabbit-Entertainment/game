48584ae4ead9d7c67209c678e905dc29b7ef133c
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-11 18:07:13 +0000

    fixed timer

--- Assets/Scripts/Utils/TimerCountdown.cs
+++ Assets/Scripts/Utils/TimerCountdown.cs
@@ -7,14 +7,16 @@ using UnityEngine.UI;
 public class TimerCountdown : MonoBehaviour {
 
     public Text timerText;
-    private GameManager gameManager;
-
-    void Start() {
-        gameManager = new GameManager();
-    }
 
     void Update() {
-        timerText.text = gameManager.TimeRemaining().ToString();
+        int secondsLeft = (int)GameManager.instance.TimeRemaining();
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft % 60;
+        string secondsStr = seconds.ToString();
+        string minutesStr = minutes.ToString();
+        if (minutes < 10) minutesStr = "0" + minutesStr;
+        if (seconds < 10) secondsStr = "0" + secondsStr;
+        timerText.text = minutesStr + ":" + secondsStr;
     }
 }
 
