0f97e076a615639cdb4250d85986f5ffc2ba9d28
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-13 17:25:07 +0000

    froze items

--- Assets/Scripts/Interactables/BasicItem.cs
+++ Assets/Scripts/Interactables/BasicItem.cs
@@ -9,6 +9,7 @@ public class BasicItem : MonoBehaviour, Interactable {
         GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
         GetComponent<BoxCollider>().enabled = false;
         GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         transform.position = pickupDestination.position;
         transform.parent = pickupDestination;
     }
@@ -16,19 +17,12 @@ public class BasicItem : MonoBehaviour, Interactable {
     public void PutDown() {
         GetComponent<BoxCollider>().enabled = true;
         GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         transform.parent = GameObject.Find("/Environment").transform;
-    }
-
-    public void Drop() {
     }
 
     // Start is called before the first frame update
     void Start() {
         gameObject.tag = "interactable";
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
     }
 }
