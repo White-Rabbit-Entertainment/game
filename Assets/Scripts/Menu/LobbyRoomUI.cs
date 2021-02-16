6965a266e1bd44036b708ef10e22badf6e55e3a2
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-16 11:13:35 +0000

    UI now shows players leaving lobby

--- Assets/Scripts/Menu/LobbyRoomUI.cs
+++ Assets/Scripts/Menu/LobbyRoomUI.cs
@@ -17,10 +17,8 @@ public class LobbyRoomUI : MonoBehaviourPun {
     public GameObject playerItemPrefab;
   
     private Hashtable props;
-    private List<string> currentPlayers;
 
     void Start() {
-      currentPlayers = new List<string>();
       seekerButton.onClick.AddListener(()=>OnJoin("Seeker"));
       robberButton.onClick.AddListener(()=>OnJoin("Robber"));
     }
@@ -30,14 +28,15 @@ public class LobbyRoomUI : MonoBehaviourPun {
     }
 
     void SetText() {
-      foreach (Player player in NetworkManager.instance.GetPlayers()) {
-        if (!currentPlayers.Contains(player.UserId)) {
-          GameObject item = Instantiate(playerItemPrefab, transform);
-          item.GetComponentInChildren<Text>().text = player.NickName;
-          item.transform.SetParent(playerList.transform);
-          currentPlayers.Add(player.UserId);
-        }
+      foreach (Transform child in playerList.transform) {
+        Destroy(child.gameObject);
       }
+      foreach (Player player in NetworkManager.instance.GetPlayers()) {
+        GameObject item = Instantiate(playerItemPrefab, transform);
+        item.GetComponentInChildren<Text>().text = player.NickName;
+        item.transform.SetParent(playerList.transform);
+      }
+      
       playerCounter.text = NetworkManager.instance.GetPlayers().Count.ToString();
     }
 
