483f8a32b987823cc02a56a308faa326f9c06884
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-17 11:55:42 +0000

    rerefixed that bug

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 
 public class PlayerSpawner : MonoBehaviour {
 
     public GameObject seekerPrefab;
     public GameObject robberPrefab;
-    
-    void Awake() {
+
+    void OnEnable() {
+    //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable() {
+    //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode) {
         if (NetworkManager.instance.LocalPlayerPropertyIs<string>("Team", "Seeker")) {
             PhotonNetwork.Instantiate(seekerPrefab.name, new Vector3(1,2,-10), Quaternion.identity);
         } else if (NetworkManager.instance.LocalPlayerPropertyIs<string>("Team", "Robber")) {
@@ -16,11 +27,5 @@ public class PlayerSpawner : MonoBehaviour {
         } else {
             Debug.Log("no team");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
