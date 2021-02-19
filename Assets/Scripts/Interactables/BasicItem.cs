ad0f168a61db689c025792df678bd8c71a401b6b
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-19 14:57:22 +0000

    housekeeping

--- Assets/Scripts/Interactables/BasicItem.cs
+++ Assets/Scripts/Interactables/BasicItem.cs
@@ -4,32 +4,4 @@ using UnityEngine;
 using Photon.Pun;
 
 public class BasicItem : PickUpable {
-    // public void PickUp(Transform pickupDestination) {
-    //     GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer); //allows multiple players to control item position
-    //     GetComponent<BoxCollider>().enabled = false;                                        
-    //     GetComponent<Rigidbody>().useGravity = false;
-    //     GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-    //     transform.position = pickupDestination.position;
-    //     transform.parent = pickupDestination;
-    // }
-
-    // public void PutDown() {
-    //     GetComponent<BoxCollider>().enabled = true;
-    //     GetComponent<Rigidbody>().useGravity = true;
-    //     GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-    //     transform.parent = GameObject.Find("/Environment").transform;
-    // }
-
-    // public void GlowOn() {
-    //     GetComponent<Outline>().enabled = true;
-    // }
-
-    // public void GlowOff() {
-    //     GetComponent<Outline>().enabled = false;
-    // }
-
-    // Start is called before the first frame update
-    // void Start() {
-    //     gameObject.tag = "interactable";
-    // }
 }
