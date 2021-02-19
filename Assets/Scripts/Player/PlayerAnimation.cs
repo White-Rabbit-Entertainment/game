83415ef5344582d53491f0634a307363bd812826
Theoajc <Theoajclarke@outlook.com>
2021-02-19 15:06:05 +0000

    Photon animations beginning to work, few bugs

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnimation : MonoBehaviourPun
 {   
 
     private Animator animator;
     public CharacterController playerController;
 
+    void Awake() {
+        // If the player is not me (ie not some other player on the network)
+        // then destory this script
+        if (!photonView.IsMine) {
+            Destroy(this);
+        }
+
+        // Dont destory a player on scene change
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
