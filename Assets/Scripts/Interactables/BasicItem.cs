cee2f92d821b66d84c94b93bfbcdc15c78ac6233
Joe Bryan <dn18923@bristol.ac.uk>
2021-02-20 14:58:44 +0000

    -

--- Assets/Scripts/Interactables/BasicItem.cs
+++ Assets/Scripts/Interactables/BasicItem.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using Photon.Pun;
 
 public class BasicItem : PickUpable {
-    
+
     [PunRPC]
     public void MakeStealable() {
         gameObject.AddComponent<StealableItem>();
+        Destroy(this);
     }
 }
