24cbbfeafe601090e1c04c5d56f3ffe5d94596d7
Isaac Wood <sd18748@bristol.ac.uk>
2021-02-24 10:34:29 +0000

    working navmesh

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using Photon.Pun;
 
 public class PlayerAnimation : MonoBehaviourPun
-{   
+{
 
     private Animator animator;
     public CharacterController playerController;
+    public string currAnimation;
 
     void Awake() {
         // If the player is not me (ie not some other player on the network)
@@ -28,18 +29,38 @@ public class PlayerAnimation : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Velocity", playerController.velocity.magnitude);
+        // animator.SetFloat("Velocity", playerController.velocity.magnitude);
 
         if (Input.GetKeyDown(KeyCode.Z)) {
             // Debug.Log("anim1");
             animator.SetTrigger("Dancing");
-            animator.ResetTrigger("Talking");
+            if (currAnimation != "Walking"){
+              animator.ResetTrigger(currAnimation);
+            } else {
+              animator.SetBool("Walking",false);
+            }
+            currAnimation = "Dancing";
         }
         if (Input.GetKeyDown(KeyCode.X)) {
-            Debug.Log("anim2");
+            // Debug.Log("anim2");
             animator.SetTrigger("Talking");
-            animator.ResetTrigger("Dancing");
-
+            if (currAnimation != "Walking"){
+              animator.ResetTrigger(currAnimation);
+            } else {
+              animator.SetBool("Walking",false);
+            }
+            currAnimation = "Talking";
+            // animator.ResetTrigger("Talking");
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            animator.SetBool("Walking",true);
+            if (currAnimation != "Walking"){
+              animator.ResetTrigger(currAnimation);
+            } else {
+              animator.SetBool("Walking",false);
+            }
+            currAnimation = "Walking";
+            // animator.SetBool("Walking",false);
         }
     }
 }
