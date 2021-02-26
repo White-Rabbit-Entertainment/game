3f4ee63957ffa7b5d32b0c7cebce81a612e18d41
James Elgar <jamesnelgar@gmail.com>
2021-02-26 17:47:44 +0000

    New Scene and AIs and Animations (#72)
    * redone footsteps with no conflicts
    
    * Photon animations beginning to work, few bugs
    
    * redone footsteps with no conflicts
    
    * animated player with proper animations and working across network, now need to do object animations
    
    * animated player with proper animations and working across network, now need to do object animations
    
    * animated player with proper animations and working across network, now need to do object animations
    
    * Added a whole load of stuff to the scene
    
    * Temporarily comment out end game
    
    * Set gold bar to transfere ownership
    
    * Sync angular velcotiy of gold bar
    
    * Can only pick up item once
    
    * Cannot interact with pickedup object
    
    * animated player with proper animations and working across network, with pickupable
    
    * animated player with proper animations and working across network, with pickupable
    
    * Added pickup rpc
    
    * Called rest conditions in putdown
    
    * Footsteps added to prefab
    
    * Only freeze on pickup client
    
    * working navmesh
    
    * Footsteps added to prefab
    
    * player spawner mulitple
    
    * Made all interactables taskable
    
    * Rewrote PrimaryInteraction
    
    * working animation
    
    * xxx
    
    * Added outline logic to taskable
    
    * Added reversable interactable
    
    * Added Character class
    
    * Added some prints
    
    * dkd
    
    * Updated prefab teams
    
    * Added HasTag for team
    
    * Move pickup logic
    
    * Moved pickup logic onto player
    
    * Actually did the things
    
    * Added animator to chest
    
    * Added photon animator
    
    * Added boolean trigger
    
    * Wathcing view
    
    * Readded open on click
    
    * xxx
    
    * Updated prefab animation
    
    * Added some random int
    
    * Its broke
    
    * added change ownership to item animation
    
    * fixed
    
    * yes
    
    * chest
    
    * fixed
    
    * commit
    
    * much perfect
    
    * Fixed cannons wheels
    
    * put pickupables back into interactables when they are put down
    
    * neater code
    
    * Fixed some merge issues
    
    * Updated agent scripts
    
    * lll
    
    * Agents working
    
    * Removed debugs
    
    * Fixed walking animation
    
    * Fixed walking animation
    
    Co-authored-by: Theoajc <Theoajclarke@outlook.com>
    Co-authored-by: Isaac Wood <sd18748@bristol.ac.uk>
    Co-authored-by: Joe Bryan <dn18923@bristol.ac.uk>

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public abstract class Character : MonoBehaviour
+{
+  public Transform pickupDestination; 
+  public PickUpable currentHeldItem; 
+
+  public Team team;
+  public bool HasItem() {
+    return currentHeldItem != null; 
+  }
+
+  public virtual void Start() {
+  }
+
+  public void PickUp(PickUpable item) {
+    currentHeldItem = item;
+    // An item can only be moved by a player if they are the owner.
+    // Therefore, give ownership of the item to the local player before
+    // moving it.
+    PhotonView view = item.GetComponent<PhotonView>();
+    view.TransferOwnership(PhotonNetwork.LocalPlayer);
+
+    item.SetItemPickupConditions();
+
+    // Move to players pickup destination.
+    item.transform.position = pickupDestination.position;
+    
+    // Set the parent of the object to the pickupDestination so that it moves
+    // with the player.
+    item.transform.parent = pickupDestination;
+  }
+  
+  public void PutDown(PickUpable item) {
+    currentHeldItem = null;
+    item.ResetItemConditions();
+
+    item.transform.parent = GameObject.Find("/Environment/Interactables").transform;
+  }
+}
