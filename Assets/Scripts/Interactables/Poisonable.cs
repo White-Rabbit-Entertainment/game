68eb7795bf4c60e084e4ba53aa8f51f86ca62587
Joe Bryan <dn18923@bristol.ac.uk>
2021-03-10 15:07:10 +0000

    changed poisonable

--- Assets/Scripts/Interactables/Poisonable.cs
+++ Assets/Scripts/Interactables/Poisonable.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 using Photon.Pun;
 
 public class Poisonable : Interactable {
-
   public Meal meal;
-
-  public override void Start() {
-    base.Start();
-    meal = gameObject.AddComponent<Meal>() as Meal;
-  }
 
   public override void Reset() {
     singleUse = true;
   }
 
   public override void PrimaryInteraction(Character player) {
-    view.RPC("Poison", RpcTarget.All);
+    PhotonView mealView = meal.GetComponent<PhotonView>();
+    mealView.RPC("Poison", RpcTarget.All);
     ((Traitor)player).hasPoison = false;
   }
 
@@ -26,7 +21,6 @@ public class Poisonable : Interactable {
   }
 
   public bool IsPoisoned() {
-    PhotonView view = GetComponent<PhotonView>();
-    return NetworkManager.instance.PlayerPropertyIs<bool>("Poisoned", true, view.Owner);
+    return meal.isPoisoned;
   }
 }
