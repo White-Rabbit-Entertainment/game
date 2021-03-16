3de99c0b551a9cad5d9b2fd6c2fe36cdc35c9ab2
James Elgar <jamesnelgar@gmail.com>
2021-03-16 16:53:29 +0000

    Set meal ownership in room property (#112)

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -63,7 +63,7 @@ public abstract class Interactable : MonoBehaviourPun {
   // The primary action to do when an item is interacted with. At the moment
   // this is when an item is clicked on.
   public virtual void PrimaryInteraction(Character character) {
-    if (HasTask() && NetworkManager.instance.LocalPlayerPropertyIs("Team", Team.Loyal)) {
+    if (HasTask() && character.canTask) {
       view.RPC("CompleteTask", RpcTarget.All);
     }
     
@@ -86,8 +86,8 @@ public abstract class Interactable : MonoBehaviourPun {
   }
   
   /// <summary> Remove glow. </summary>
-  public void GlowOff() {
-    if (HasTask() && NetworkManager.instance.LocalPlayerPropertyIs("Team", Team.Loyal)) {
+  public void GlowOff(PlayableCharacter character) {
+    if (HasTask() && character.canTask) {
       outline.OutlineColor = taskColour;
     } else {
       outline.enabled = false;
@@ -109,7 +109,7 @@ public abstract class Interactable : MonoBehaviourPun {
       task.description = taskDescription;
 
       // Set outline colour and turn on
-      if (NetworkManager.instance.LocalPlayerPropertyIs("Team", Team.Loyal)) {
+      if (Team.Loyal.HasFlag(NetworkManager.instance.GetLocalPlayerProperty<Team>("Team"))) {
         outline.OutlineColor = taskColour;
         outline.enabled = true;
       }
