aa5a08d61b1fc05c423364e6f230c220314517c9
jelgar <jamesnelgar@gmail.com>
2021-03-17 09:28:03 +0000

    Added slider

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    private bool menuOpen = false;
+
+    public Slider mouseSensitivitySlider;
+    public GameObject settingsPanel;
+
+    // Start is called before the first frame update
+    void Start() {
+        mouseSensitivitySlider.onValueChanged.AddListener (delegate {ValueChangeCheck ();}); 
+    }
+    
+    public void OpenMenu() {
+      bool menuOpen = true;
+      settingsPanel.SetActive(true);
+      Cursor.lockState = CursorLockMode.None;
+    }
+    
+    public void CloseMenu() {
+      settingsPanel.SetActive(false);
+      Cursor.lockState = CursorLockMode.Locked;
+      bool menuOpen = false;
+    }
+    
+    public void ValueChangeCheck() {
+      Debug.Log(mouseSensitivitySlider.value);
+    }
+
+    public void Update() {
+      if (Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.P)){
+        OpenMenu();
+      }
+    }
+}
