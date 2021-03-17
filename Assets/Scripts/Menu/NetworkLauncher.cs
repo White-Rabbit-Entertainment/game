4b07a9ea5adaf02961168a37f66a3252361167b5
ZF-u <57496395+ZF-u@users.noreply.github.com>
2021-03-17 10:44:26 +0000

    Feature/64 (#76)
    * 64 demand3
    
    * chat box added
    
    * 64 random roomName
    
    * Integrate chat and login
    
    Please ignore Hall
    
    * No Pub and Pri at the moment
    
    P: LobbyRoomUI.OnRoomListUpdate: no suitable method found override.
    
    * Click room + private room
    
    No errror, but
    1.There is no problem with the code, but I don't know why the room is not listed. It is useful before the merger, it is not clear if it is a local issue. Follow up.
    2.The current private room is more like creating a room with a special prefix name, and then filtering it out in the room list.
    
    * Working on merge
    
    * Confused
    
    * fix room list can not show
    
    * Removed commented out code
    
    * fix click
    
    problem:
    when u join in room, can only show host's name and messages
    
    Co-authored-by: yuqing zhu <sh18248@bristol.ac.uk>
    Co-authored-by: jelgar <jamesnelgar@gmail.com>

---
+++ Assets/Scripts/Menu/NetworkLauncher.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+
+
+public class NetworkLauncher : MonoBehaviourPunCallbacks
+{
+    public GameObject loginUI;
+    public GameObject nameUI;
+    public InputField playerName;
+    //public InputField roomName;
+
+    public GameObject roomListUI;
+    //public GameObject chatUI;
+
+
+
+    public string roomName()
+    {
+        int number;
+        char code;
+        string roomString = string.Empty;
+        //Random random = new Random();
+        for (int i = 0; i < 8; i++)
+        {
+            number = Random.Range(0, 10);
+                if (number % 2 == 0)
+                code = (char)('0' + (char)(number % 10));
+            else
+                code = (char)('A' + (char)(number % 26));
+            roomString += code.ToString();
+        }
+
+        return roomString;
+    }
+    
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        nameUI.SetActive(true);
+        Debug.Log("Connected to the Master");
+        PhotonNetwork.JoinLobby();
+
+    }
+
+    public void PlayButtom()
+    {
+        nameUI.SetActive(false);
+        PhotonNetwork.NickName = playerName.text;
+        loginUI.SetActive(true);
+        if (PhotonNetwork.InLobby)
+        {
+            roomListUI.SetActive(true);
+        }
+    }
+
+
+    public void JoinOrCreateButton()
+    {
+        //if (roomName.text.Length < 2)
+        //    return;
+
+
+        roomListUI.SetActive(false);
+        loginUI.SetActive(false);
+
+        RoomOptions options = new RoomOptions { MaxPlayers = 6 };
+        //PhotonNetwork.JoinOrCreateRoom(roomName.text, options, default);
+        PhotonNetwork.JoinOrCreateRoom(roomName(), options, default);
+
+        //chatUI.SetActive(true);
+
+
+    }
+
+    //public override void OnJoinedRoom()
+    //{
+    //    PhotonNetwork.LoadLevel(1);
+    //}
+
+}
