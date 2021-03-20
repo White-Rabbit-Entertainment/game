ce3b8b54209d09717ab623d5ffc14a85a3098aaa
jelgar <jamesnelgar@gmail.com>
2021-03-20 12:02:02 +0000

    Readded traitors and click on kill

--- Assets/Scripts/Interactables/Poisonable.cs
+++ Assets/Scripts/Interactables/Poisonable.cs
@@ -20,7 +20,7 @@ public class Poisonable : Interactable {
     PhotonView mealView = player.GetMeal().GetComponent<PhotonView>();
     mealView.RPC("Poison", RpcTarget.All);
     ((Traitor)poisoningPlayer).UsePoison();
-    poisoningPlayer.GetComponent<PhotonView>().RPC("Kill", poisoningPlayer.owner);
+    GetComponent<PhotonView>().RPC("Kill", GetComponent<PhotonView>().Owner);
   }
 
   public override bool CanInteract(Character character) {
