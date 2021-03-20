e690744669aed5acf773a80c7b848580cb892e85
jelgar <jamesnelgar@gmail.com>
2021-03-20 16:03:37 +0000

    Stuff

--- Assets/Scripts/UI/PlayersUI.cs
+++ Assets/Scripts/UI/PlayersUI.cs
@@ -29,7 +29,7 @@ public class PlayersUI : MonoBehaviourPun {
    
     // If the player is dead cross them out
     if (player is Ghost) {
-      Transform cross = transform.Find("Cross");
+      Transform cross = transform.FindChild("Cross");
       cross.gameObject.SetActive(true);
     }
   }
