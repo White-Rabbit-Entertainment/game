802608531b9ab82ada7d1060158ff4b2109957d6
Theoajc <Theoajclarke@outlook.com>
2021-03-26 14:22:04 +0000

    Sabotageable

--- Assets/Scripts/Interactables/Sabotageable.cs
+++ Assets/Scripts/Interactables/Sabotageable.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Sabotageable : Interactable {
+
+    public bool isSabotaged;
+
+    public int initialNumberOfPlayersToFix;
+
+    public int numberOfPlayersToFix;
+
+    // private void Dictionary<string, string> openWith = new Dictionary<string, string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isSabotaged = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // public override bool CanInteract(Character character) {
+    //     if (character is Traitor && Timer.Sabotage.IsStarted()) return false;
+    //     return base.CanInteract(character);
+    // }
+
+    private void Reset() {
+        team = Team.Traitor;
+        taskTeam = Team.Real;
     }
+    public override void PrimaryInteraction(Character character) {
+        if (!isSabotaged && character.team == Team.Traitor) {
+            //Timer.Sabotage.IsStarted();
+            Sabotage();
+            view.RPC("AddTaskRPC", RpcTarget.All);
+        } else if (isSabotaged && !(character.team == Team.Traitor)) {
+            Fix();
+            view.RPC("CompleteTask", RpcTarget.All);
+
+        }
+    }
+    void Sabotage() {
+        GetComponent<PhotonView>().RPC("SabotageRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+	void SabotageRPC() {
+      isSabotaged = true;
+	}
+
+    void Fix() {
+        GetComponent<PhotonView>().RPC("FixRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+	void FixRPC() {
+      numberOfPlayersToFix--;  
+      if (numberOfPlayersToFix == 0) isSabotaged = false;
+	}
 }
