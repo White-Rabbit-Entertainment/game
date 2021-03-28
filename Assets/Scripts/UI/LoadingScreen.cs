73f1f246d1f897db678f4a66e7455d4af53b412b
Isaac Wood <sd18748@bristol.ac.uk>
2021-03-28 19:42:05 +0100

    changed loading screen

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using Photon.Pun;
 
 public class LoadingScreen : MonoBehaviour {
-  
+
   public GameObject loadingScreen;
-  public PlayersUI playersUI; 
-  public Text text; 
+  public PlayersUI playersUI;
+  public Text text;
   public Button closeButton;
 
-  public string traitorDescription;
-  public string loyalDescription;
+  [TextArea(15,20)]
+  private string traitorDescription = "crew's aim is to stop the Loyal crew to complete their tasks whilst not being found out.";
+  private string loyalDescription = "crew's aim is to complete your tasks in a given time, if you do so they win the game. All crew members will also have the opportunity to vote a player off the ship if they believe them to be a traitor.";
 
   private Dictionary<Team, string> descriptions;
 
@@ -24,7 +25,7 @@ public class LoadingScreen : MonoBehaviour {
     descriptions.Add(Team.Loyal, loyalDescription);
     descriptions.Add(Team.Traitor, traitorDescription);
 
-    text.text = $"Welcome {PhotonNetwork.LocalPlayer.NickName}! You are a {team}. {descriptions[team]}";
+    text.text = $"Welcome to SS White Rabbit, {PhotonNetwork.LocalPlayer.NickName}! You are part of the {team} crew. The {team} {descriptions[team]}";
 
     closeButton.onClick.AddListener(()=>CloseMenu());
   }
