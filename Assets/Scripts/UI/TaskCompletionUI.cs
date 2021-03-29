84913d27c1b298f135a1552d8d501d37d8178048
jelgar <jamesnelgar@gmail.com>
2021-03-29 11:05:55 +0100

    Added loading bar

--- Assets/Scripts/UI/TaskCompletionUI.cs
+++ Assets/Scripts/UI/TaskCompletionUI.cs
@@ -5,8 +5,10 @@ using TMPro;
 public class TaskCompletionUI : MonoBehaviour {
   public GameObject taskCompletionBar;
   public TaskManager taskManager;
+  public Slider slider;
 
   public void Update() {
     taskCompletionBar.GetComponentInChildren<Text>().text = $"{taskManager.NumberOfTasksCompleted()} / {taskManager.tasks.Count}";
+    slider.value = taskManager.NumberOfTasksCompleted() / taskManager.tasks.Count;
   }
 }
