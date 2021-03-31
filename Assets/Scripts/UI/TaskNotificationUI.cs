7aa07b34f4321a52a62597ca5f5e90df6a6a6e0b
Theoajc <Theoajclarke@outlook.com>
2021-03-31 16:52:29 +0100

    task complete pops up

---
+++ Assets/Scripts/UI/TaskNotificationUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TaskNotificationUI : MonoBehaviour
+{
+
+    public TextMeshProUGUI notificationPrefab;
+    public string taskCompleteMessage;
+    public string taskUndoneMessage;
+
+    public void SetNotification(bool isComplete) {
+        if (isComplete) {
+            notificationPrefab.SetText(taskCompleteMessage);
+        } else {
+            notificationPrefab.SetText(taskUndoneMessage);
+        }
+
+        StartCoroutine(RemoveNotification(2f));
+    }
+
+
+    IEnumerator RemoveNotification(float timeInSeconds) {
+        yield return new WaitForSeconds(timeInSeconds);
+        notificationPrefab.SetText("");
+    }
+    
+}
