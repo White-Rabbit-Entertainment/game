18902ad123d9cbf21059bf09adf39959a6aac4ee
Isaac Wood <sd18748@bristol.ac.uk>
2021-04-02 11:03:10 +0100

    clock change

--- Assets/Scripts/UI/TimerCountdown.cs
+++ Assets/Scripts/UI/TimerCountdown.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimerCountdown : MonoBehaviour {
 
-    public Text timerText;
+    public TextMeshProUGUI timerText;
 
     void Update() {
         timerText.text = Timer.RoundTimer.FormatTime();
