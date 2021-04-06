800b438f1939efb9112f6d096dc0a0d31635c717
jelgar <jamesnelgar@gmail.com>
2021-04-06 12:57:32 +0100

    Back button scripts

---
+++ Assets/Scripts/Menu/BackButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour {
+   
+    public GameObject currentGO;   // Where you are
+    public GameObject backGO;      // Where you go 
+
+    public bool changeScene = false;
+    public string newScene;
+
+    Button button;
+
+    void Start() {
+        button = GetComponent<Button>(); 
+        button.onClick.AddListener(GoBack);
+    }
+
+    void GoBack() {
+        if (changeScene) {
+            NetworkManager.instance.ChangeScene(newScene);
+        } else {
+            currentGO.SetActive(false);
+            backGO.SetActive(true);
+        }
+    }
+}
