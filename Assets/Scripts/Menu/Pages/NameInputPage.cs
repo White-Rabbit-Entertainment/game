41d2796a1ae08cc0df4e73ec1dd18e98a6d6b81a
Joe Bryan <dn18923@bristol.ac.uk>
2021-04-07 17:05:11 +0100

    can now only use alphanumeric names

--- Assets/Scripts/Menu/Pages/NameInputPage.cs
+++ Assets/Scripts/Menu/Pages/NameInputPage.cs
@@ -1,20 +1,29 @@
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using System.Text.RegularExpressions;
 
 public class NameInputPage : MenuPage {
     public TMP_InputField playerNameInput;
     public Button startButton;
     public JoinRoomPage joinRoomPage;
+    string pattern = @"^[a-zA-Z0-9]+$";
+    Regex inputChecker;
     
     void Start() {
+        inputChecker = new Regex(pattern);
         playerNameInput.Select();
         playerNameInput.ActivateInputField(); 
         startButton.onClick.AddListener(StartButton);
     }
     
     void StartButton() {
-        PhotonNetwork.LocalPlayer.NickName = playerNameInput.text;
-        joinRoomPage.Open();
+        if (inputChecker.IsMatch(playerNameInput.text)) {
+            PhotonNetwork.LocalPlayer.NickName = playerNameInput.text;
+            joinRoomPage.Open();
+        } else {
+            Debug.Log("Please enter a valid name");
+        }    
     }
 }
