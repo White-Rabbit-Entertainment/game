db9b228559dcf8f4f3c4a769a985da5f4df4aa3a
Theoajc <Theoajclarke@outlook.com>
2021-04-12 12:22:01 +0100

    floodable

---
+++ Assets/Scripts/Interactables/Floodable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Floodable : Sabotageable
+{
+    public GameObject water;
+
+    [PunRPC]
+    public override void Sabotage() {
+        base.Sabotage();
+        water.SetActive(true);
+    }
+
+    [PunRPC]
+    public override void Fix(int fixPlayerViewId) {   
+        base.Fix(fixPlayerViewId);
+        if (!isSabotaged) water.SetActive(false);
+    }
+}
+
