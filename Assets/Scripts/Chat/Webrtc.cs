46739888c1b9602e849a1ec8334d4d5220f3ec24
jelgar <jamesnelgar@gmail.com>
2021-04-14 07:58:03 +0100

    Added Init for webrtc

--- Assets/Scripts/Chat/Webrtc.cs
+++ Assets/Scripts/Chat/Webrtc.cs
@@ -2,40 +2,15 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 
 public class Webrtc : MonoBehaviour {
-
+    
     [DllImport("__Internal")]
-    private static extern void Hello();
-
+    private static extern void SetupLocalStream();
+    
     [DllImport("__Internal")]
-    private static extern void HelloString(string str);
-
-    [DllImport("__Internal")]
-    private static extern void PrintFloatArray(float[] array, int size);
-
-    [DllImport("__Internal")]
-    private static extern int AddNumbers(int x, int y);
-
-    [DllImport("__Internal")]
-    private static extern string StringReturnValueFunction();
-
-    [DllImport("__Internal")]
-    private static extern void BindWebGLTexture(int texture);
+    private static extern void Init();
 
     void Start() {
-        Hello();
-        
-        HelloString("This is a string.");
-        
-        float[] myArray = new float[10];
-        PrintFloatArray(myArray, myArray.Length);
-        
-        int result = AddNumbers(5, 7);
-        Debug.Log(result);
-        
-        Debug.Log(StringReturnValueFunction());
-        
-        var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
-        BindWebGLTexture(texture.GetNativeTextureID());
+        Init();
     }
 }
 
