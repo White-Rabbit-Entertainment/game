f9ced9ded0d26571eab4adf4d13000a7da00bbc7
James Elgar <jamesnelgar@gmail.com>
2021-04-16 16:05:37 +0100

    Merged main and feature/fire (#150)

---
+++ Assets/Scripts/Interactables/Flammable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Flammable : Sabotageable
+{
+    public GameObject smallFires;
+
+    [PunRPC]
+    public override void Sabotage() {
+        base.Sabotage();
+        smallFires.SetActive(true);
+    }
+
+    [PunRPC]
+    public override void Fix(int fixPlayerViewId) {   
+        base.Fix(fixPlayerViewId);
+        if (!isSabotaged) smallFires.SetActive(false);
+    }
+}
