a42eabfa3569dc74bfcde475c5e23680369150a5
James Elgar <jamesnelgar@gmail.com>
2021-04-16 14:38:15 +0100

    Update Assets/Scripts/CameraScript.cs

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -18,31 +18,3 @@ public class CameraScript : MonoBehaviour
 
     }
 }
-
-
-// using Microsoft.CSharp.RuntimeBinder.Binder.Convert;
-
-// public class CameraScript : MonoBehaviour {
-// 	void Start () {
-//         // var options = new Dictionary<string, object>();
-//         // options["Arguments"] = new[] {"--onnx", "--opt", "3d"};
-//         var engine = Python.CreateEngine();
-
-//         ICollection<string> searchPaths = engine.GetSearchPaths();
-
-//         //Path to the folder of greeter.py
-//         searchPaths.Add("../../year3/Game/3DDFA_V2");
-//         //Path to the Python standard library
-//         searchPaths.Add("../Plugins/Lib/");
-//         Debug.Log(searchPaths);
-//         engine.SetSearchPaths(searchPaths);
-        
-//         // List<string> argv = new List<string>();
-//         // argv.Add("--onnx --opt 3d");
-//         // engine.GetSysModule().SetVariable("argv", argv);
-
-//         engine.ExecuteFile("../../year3/Game/3DDFA_V2/test_unity.py");
-//         // GetComponent<RawImage>().texture = py;
-        
-//     }
-// }
