617b19b9d0bfd944f930aa37bd0d64b15558f775
ZF-u <57496395+ZF-u@users.noreply.github.com>
2021-04-17 15:56:08 +0100

    1

---
+++ Assets/Scripts/Interactables/Filled.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Filled : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            this.transform.tag = "Filled";
+        }
+    }
+}
