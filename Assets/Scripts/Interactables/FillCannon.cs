61dec4d666be8fcf95a546dae73e94b33aa5eca7
ZF-u <57496395+ZF-u@users.noreply.github.com>
2021-04-17 13:04:23 +0100

    1

--- Assets/Scripts/Interactables/FillCannon.cs
+++ Assets/Scripts/Interactables/FillCannon.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class FillCannon : Pickupable
 {
-    
+    public override void Reset()
+    {
+        taskDescription = "Fill cannon by " + this.name;
+        base.Reset();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.collider.tag = )
+        //{
+
+        //}
+    }
 }
