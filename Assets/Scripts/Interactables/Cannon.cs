d338d65c13f4ebeba3f10055c33798dc2b09b0b4
jelgar <jamesnelgar@gmail.com>
2021-05-04 21:57:46 +0100

    Cannons

---
+++ Assets/Scripts/Interactables/Cannon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : Interactable {
+
+    // Cannons will always have 1 cannonball requirement
+    CannonBall CannonBall {
+        get { return task.requirements[0].GetComponent<CannonBall>(); }
+    }
+
+    public override bool CanInteract(Character character) {
+        if (character is Traitor && task != null && CannonBall.task.isCompleted) {
+            Debug.Log("Cann interact");
+            return true;
+        }
+        Debug.Log("Cannon interact");
+        return false;
+    }
+}
