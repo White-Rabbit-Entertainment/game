2810d5446372ef84a04c93c437a6e4186ff1debb
jelgar <jamesnelgar@gmail.com>
2021-05-05 09:50:45 +0100

    More cannon stuff

--- Assets/Scripts/Interactables/Cannon.cs
+++ Assets/Scripts/Interactables/Cannon.cs
@@ -6,11 +6,16 @@ public class Cannon : Interactable {
 
     // Cannons will always have 1 cannonball requirement
     CannonBall CannonBall {
-        get { return task.requirements[0].GetComponent<CannonBall>(); }
+        get { 
+            if (task != null && task.requirements.Count > 0) {
+                return task.requirements[0].GetComponent<CannonBall>(); 
+            }
+            return null;
+        }
     }
 
     public override bool CanInteract(Character character) {
-        if (character is Traitor && task != null && CannonBall.task.isCompleted) {
+        if (character is Traitor && CannonBall != null && CannonBall.task.isCompleted) {
             Debug.Log("Cann interact");
             return true;
         }
