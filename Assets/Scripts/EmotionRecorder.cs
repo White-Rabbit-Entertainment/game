3a5e28b651a4832b95114c2132b00c324d3d3aac
Theoajc <Theoajclarke@outlook.com>
2021-05-13 14:15:28 +0100

    fixes

---
+++ Assets/Scripts/EmotionRecorder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnityExample;
+
+public class EmotionRecorder : MonoBehaviour
+{
+    public string lastEmotion;
+
+    [SerializeField] private AsynchronousFaceDetectionWebCamTextureExample asynchronousFaceDetectionWebCamTextureExample;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastEmotion = "";
+        // asynchronousFaceDetectionWebCamTextureExample = GetComponent<AsynchronousFaceDetectionWebCamTextureExample>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        if (asynchronousFaceDetectionWebCamTextureExample.lastEmotion != lastEmotion) {
+            lastEmotion = asynchronousFaceDetectionWebCamTextureExample.lastEmotion;
+            NetworkManager.instance.GetMe().SetEmotion(lastEmotion);
+            Debug.Log("last emotion:" +  lastEmotion);
+        }
+    }
+}
