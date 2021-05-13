0db22aca53d2ebfd012288dd154920ba276ebf18
Theoajc <Theoajclarke@outlook.com>
2021-05-13 15:37:29 +0100

    fixes

--- Assets/Scripts/EmotionRecorder.cs
+++ Assets/Scripts/EmotionRecorder.cs
@@ -1,28 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using OpenCVForUnityExample;
+using OpenCVForUnityExample;
 
 public class EmotionRecorder : MonoBehaviour
 {
     public string lastEmotion;
 
-    // [SerializeField] private AsynchronousFaceDetectionWebCamTextureExample asynchronousFaceDetectionWebCamTextureExample;
+    [SerializeField] private AsynchronousFaceDetectionWebCamTextureExample asynchronousFaceDetectionWebCamTextureExample;
     // Start is called before the first frame update
     void Start()
     {
         lastEmotion = "";
-        // asynchronousFaceDetectionWebCamTextureExample = GetComponent<AsynchronousFaceDetectionWebCamTextureExample>();
+        asynchronousFaceDetectionWebCamTextureExample = GetComponent<AsynchronousFaceDetectionWebCamTextureExample>();
 
     }
 
     // Update is called once per frame
     void Update()
     {       
-        // if (asynchronousFaceDetectionWebCamTextureExample.lastEmotion != lastEmotion) {
-        //     lastEmotion = asynchronousFaceDetectionWebCamTextureExample.lastEmotion;
-        //     NetworkManager.instance.GetMe().SetEmotion(lastEmotion);
-        //     Debug.Log("last emotion:" +  lastEmotion);
-        // }
+        if (asynchronousFaceDetectionWebCamTextureExample.lastEmotion != lastEmotion) {
+            lastEmotion = asynchronousFaceDetectionWebCamTextureExample.lastEmotion;
+            NetworkManager.instance.GetMe().SetEmotion(lastEmotion);
+            Debug.Log("last emotion:" +  lastEmotion);
+        }
     }
 }
