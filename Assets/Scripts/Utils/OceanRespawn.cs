d89e8c1c50162a4cb437255ad3512acb72bd6386
jelgar <jamesnelgar@gmail.com>
2021-05-14 10:49:30 +0100

    OceanRespawn

---
+++ Assets/Scripts/Utils/OceanRespawn.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanRespawn : MonoBehaviour {
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider) {
+       Debug.Log($"Something hit the sea, {collider.gameObject}");
+    }
+}
